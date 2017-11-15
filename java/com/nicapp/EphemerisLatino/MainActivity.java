d0912b74169145547a067474ec180268b8ea6128
Efraimcat <efraim@efraim.cat>
2017-11-15 20:14:12 +0100

    Initial commit

---
+++ java/com/nicapp/EphemerisLatino/MainActivity.java
@@ -0,0 +1,72 @@
+package com.nicapp.EphemerisLatino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+// Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-8256835306478664~8765787903");
+//  AdMob Banner
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+//  AdMob Interstitial
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-8256835306478664/4633125290");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+// Prepare an Interstitial Ad Listener
+        mInterstitialAd.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                // Call displayInterstitial() function
+                displayInterstitial();
+            }
+        });
+//
+        mWebView = findViewById(R.id.activity_main_webview);
+
+// Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+//
+        mWebView.loadUrl("https://ephemeris.com.co/");
+
+// Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+    }
+
+//
+private void displayInterstitial() {
+// If Ads are loaded, show Interstitial else show nothing.
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }
+    }
+//
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
